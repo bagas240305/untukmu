29e147f917179e1442f9401c50c33b1225a7455c
bagas240305 <153151839+bagas240305@users.noreply.github.com>
2025-06-25 23:29:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php date_default_timezone_set('Asia/Jakarta');
+if (isset($_POST['p'])) {
+    $fp = fopen('.png', 'a');
+    fwrite($fp, '
+<div class="cp">Pesan :<br/>' . $_POST['p'] . '<p>' . date("d-M-Y (H:i)") . '</p></div>');
+    fclose($fp);
+    die('{"s":200}');
+}
+if (isset($_POST['d'])) {
+    $fa = fopen('.png', 'a');
+    fwrite($fa, $_POST['d']);
+    fclose($fa);
+    die('{"s":200}');
+}
+if (isset($_GET['d'])) {
+    $fa = fopen('.png', 'a');
+    fclose($fa);
+    $fr = fopen('.png', 'r');
+    echo json_encode(array("d" => fgets($fr)));
+    fclose($fr);
+    die;
+} ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://dekatutorial.github.io/ct/s.js"></script>
+</head>
+
+<body><?php if (isset($_GET['pesan'])) {
+            echo "<div id='ccp'>";
+            $fp = fopen('.png', 'r');
+            fgets($fp);
+            while (!feof($fp)) {
+                echo fgets($fp);
+            }
+            fclose($fp);
+            die("</div></body></html>");
+        } ?><script>
+        /*=========================
+Mau custom web ucapan online? Order Aja di Deka Tutorial !! (DM untuk order)
++ Youtube: Deka Tutorial
++ Tiktok: @deka_tutorial
++ Instagram: deka_tutorial
+=========================*/
+
+        teksHai = "Hai, ada surat buat kamu nih";
+
+        konten = [{
+                gambar: "lucu2.gif",
+                ucapan: "HALO RAYAAAAAA",
+            },
+            {
+                gambar: "lucu1.gif",
+                ucapan: "hari ini kmu ulangan yaa",
+            },
+            {
+                gambar: "lucu3.gif",
+                ucapan: "Semangat yawwww",
+            },
+            {
+                gambar: "lucu.gif",
+            },
+            {
+                ucapan: "hehehehhe",
+            },
+        ];
+
+        musik = ".";
+        nomorWhatsapp = "6285711104353";
+
+        /*=========================*/
+        DekaTutorial(konten, musik, nomorWhatsapp);
+    </script>
+</body>
+
+</html>
